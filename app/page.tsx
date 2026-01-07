1f67e72f72409b955bcb52c9b25cd0fd85001d4f
Nabeel John <nabeeljohn@outlook.com>
2026-01-07 18:06:55 -0500

    Installed tailscale

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "NJohn Web - Home"
+};
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Nabeel John | Coming Soon
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white text-center px-4">
+        Coming Soon - Nabeel John
+      </h1>
     </div>
-  );
+  )
 }
