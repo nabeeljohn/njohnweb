c5538cf05ce832f25b5d7998b843611e2531a48b
Nabeel John <nabeeljohn@outlook.com>
2026-01-28 22:48:16 -0500

    remove Hi

--- app/tools/todo/Todocard.tsx
+++ app/tools/todo/Todocard.tsx
@@ -3,7 +3,7 @@ import classes from "./TodoCard.module.css";
 export default function TodoCard(props: {userName: string, body: string}) {
     return (
         <div className="max-w-md mx-auto my-4 p-6 bg-gray-100 rounded-lg shadow-md text-black">
-            <p className={classes.username}>Hi, {props.userName}</p>
+            <p className={classes.username}>{props.userName}</p>
             <p>{props.body}</p>
         </div>    
     );
