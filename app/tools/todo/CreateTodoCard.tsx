241842d029cfb575c3df1dbc9223deb580ab8c59
Nabeel John <nabeeljohn@outlook.com>
2026-01-29 23:10:23 -0500

    onchange and useState addition

--- app/tools/todo/CreateTodoCard.tsx
+++ app/tools/todo/CreateTodoCard.tsx
@@ -1,12 +1,22 @@
+"use client";
+
+import {useState} from "react";
 import classes from "./CreateTodoCard.module.css";
 
 export default function CreateTodoCard() {
+    const [title, setTitle] = useState('');
+
+    function titleChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        setTitle(event.target.value);
+    }
+
     return (
         <form className={classes.form}>
             <div>
-                <label htmlFor="userName">Name:</label>
-                <input type="text" id="userName" name="userName" /> 
+                <label htmlFor="title">Title:</label>
+                <input type="text" id="title" name="title" onChange={titleChangeHandler} /> 
             </div>
+            <div>{title}</div>
             <div>
                 <label htmlFor="body">List Item:</label>    
                 <input type="text" id="body" name="body" />
