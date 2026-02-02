6c668fbc2538f155bf569d4b029e0f85b3186816
Nabeel John <nabeeljohn@outlook.com>
2026-02-02 18:16:24 -0500

    add logic for submit

--- app/tools/todo/CreateTodoCard.tsx
+++ app/tools/todo/CreateTodoCard.tsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
 import classes from "./CreateTodoCard.module.css";
 
-export default function CreateTodoCard({onCardTitleChange, onCardBodyChange, onCancel}:
-    {onCardTitleChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-     onCardBodyChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-     onCancel: () => void}) {
+export default function CreateTodoCard({onCancel}: { onCancel: () => void }) {
+
+    const [cardTitle, setCardTitle] = useState('AlexG');
+    const [cardBody, setCardBody] = useState('Goto Honda');
+
+    function cardTitleChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        setCardTitle(e.target.value);
+    }
+
+    function cardBodyChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        setCardBody(e.target.value);
+    }
+
+    function submitTodoEntry(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+        const toDoPost = {
+            title: cardTitle,
+            body: cardBody
+        };
+        console.log('New ToDo Entry:', toDoPost);
+        onCancel(); // Close the modal after submission
+    }
+
     return (
         <form className={classes.form}>
             <div>
                 <label htmlFor="title">Title:</label>
-                <input type="text" id="title" name="title" onChange={onCardTitleChange} /> 
+                <input type="text" id="title" name="title" onChange={cardTitleChangeHandler} />
             </div>
             <div>
-                <label htmlFor="body">List Item:</label>    
-                <input type="text" id="body" name="body" onChange={onCardBodyChange} />
+                <label htmlFor="body">List Item:</label>
+                <input type="text" id="body" name="body" onChange={cardBodyChangeHandler} />
             </div>
             <p className={classes.actions}>
-                <button type="submit">Add Item</button>
+                <button type="submit" onClick={submitTodoEntry}>Add Item</button>
                 <button type="button" onClick={onCancel}>Cancel</button>
             </p>
         </form>
--- app/tools/todo/TodoCardList.tsx
+++ app/tools/todo/TodoCardList.tsx
@@ -5,32 +5,15 @@ import Modal from "./Modal";
 import CreateTodoCard from "./CreateTodoCard";
 import classes from "./TodoCardList.module.css";
 
-import { useState } from "react";
-
 export default function TodoCardList({isPosting, isNotPosting}:
     {isPosting: boolean,
      isNotPosting: () => void}) {
-    const [cardTitle, setCardTitle] = useState('AlexG');
-    const [cardBody, setCardBody] = useState('Goto Honda');
-
-    function cardTitleChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-        setCardTitle(e.target.value);
-    }
-
-    function cardBodyChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-        setCardBody(e.target.value);
-    }
-
     return (
         <>
             {isPosting && <Modal onClose={isNotPosting}>
-                <CreateTodoCard
-                    onCardTitleChange={cardTitleChangeHandler}
-                    onCardBodyChange={cardBodyChangeHandler}
-                    onCancel={isNotPosting} />
+                <CreateTodoCard onCancel={isNotPosting} />
             </Modal>}
             <ul className={classes.todoCardList}>
-                <TodoCard cardTitle={cardTitle} cardBody={cardBody} />
                 <TodoCard cardTitle="Bob" cardBody="Need to bring Honda to the dealer" />
                 <TodoCard cardTitle="Charlie" cardBody="Need to Nissan car to the dealer" />
                 <TodoCard cardTitle="David" cardBody="Need to Chevrolet car to the dealer" />
