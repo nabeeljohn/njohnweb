025eb3a47997eee05f7b1a009b0546860c9706cd
Nabeel John <nabeeljohn@outlook.com>
2026-02-05 01:32:31 -0500

    header adjustment

--- app/tools/todo/MainHeader.tsx
+++ app/tools/todo/MainHeader.tsx
@@ -1,19 +1,25 @@
-import { MdPostAdd, MdMessage } from 'react-icons/md';
+import { MdPostAdd, MdChecklist } from 'react-icons/md';
 import classes from './MainHeader.module.css';
 
-export default function MainHeader({onCreatePost}: {onCreatePost: () => void}) {
+export default function MainHeader({ onCreatePost }: { onCreatePost: () => void }) {
     return (
         <header className={classes.header}>
-            <h1 className={classes.logo}>
-                <MdMessage />
-                 NextUp - Todo List v1.0
-            </h1>
-            <p>
-                <button className={classes.button} onClick={onCreatePost}>
-                    <MdPostAdd size={18} />
-                    Create New Todo
-                </button>
-            </p>
+            <div className={classes.logoContainer}>
+                <h1 className={classes.logo}>
+                    <MdChecklist size={50} className={classes.icon} />
+                    <span className={classes.titleText}>
+                        NextUp
+                        <span className={classes.slogan}>Organize your day, one todo at a time</span>
+                    </span>
+                </h1>
+
+            </div>
+
+            <button className={classes.button} onClick={onCreatePost}>
+                <MdPostAdd size={18} />
+                New Todo Item
+            </button>
         </header>
+
     );
 }
