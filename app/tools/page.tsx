a0228f26ae4f01bb143b28f17dcbcbe89d945e38
Nabeel John <nabeeljohn@outlook.com>
2026-02-18 17:36:22 -0500

    add jwt generator placeholder

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,6 +1,7 @@
 import DevToolsHeader from "@/appcomponents/devtools/devtoolsheader";
 import Link from "next/link";
 
+const cardStyle = "bg-gray-800 hover:bg-gray-900 text-center p-6 rounded-lg shadow-lg transition"
 export const metadata = {
   title: "NJohn Web - Developer Tools Home",
 };
@@ -13,7 +14,7 @@ export default function DevToolsHome() {
         <div className="max-w-7xl mx-auto px-6">
 
           {/* Page Title */}
-          <h1 className="text-3xl font-bold mb-8">Developer Tools</h1>
+          <h1 className="text-3xl font-bold mb-8">Developer Toolbox</h1>
 
           {/* Tools Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -21,7 +22,7 @@ export default function DevToolsHome() {
             {/* URL Encoder/Decoder */}
             <Link
               href="/tools/url"
-              className="bg-gray-800 hover:bg-gray-900 text-center p-6 rounded-lg shadow-lg transition"
+              className={cardStyle}
             >
               <h2 className="text-xl font-semibold text-blue-400 mb-2">
                 Link Lab
@@ -34,7 +35,7 @@ export default function DevToolsHome() {
             {/* Todo List */}
             <Link
               href="/tools/todo"
-              className="bg-gray-800 hover:bg-gray-900 text-center p-6 rounded-lg shadow-lg transition"
+              className={cardStyle}
             >
               <h2 className="text-xl font-semibold text-blue-400 mb-2">
                 Todo List
@@ -44,13 +45,18 @@ export default function DevToolsHome() {
               </p>
             </Link>
 
-            {/* Placeholder 1 */}
-            <div className="bg-gray-800 text-center p-6 rounded-lg shadow-lg opacity-60 cursor-not-allowed">
-              <h2 className="text-xl font-semibold text-gray-400 mb-2">
-                Tool Placeholder
+            {/* JWT Generator */}
+            <Link
+              href="/tools/jwt"
+              className={cardStyle}
+            >
+              <h2 className="text-xl font-semibold text-blue-400 mb-2">
+                JWT Generator
               </h2>
-              <p className="text-gray-500 text-sm">Coming soon…</p>
-            </div>
+              <p className="text-gray-300 text-sm">
+                Generate and sign JWT tokens.
+              </p>
+            </Link>
 
             {/* Placeholder 2 */}
             <div className="bg-gray-800 text-center p-6 rounded-lg shadow-lg opacity-60 cursor-not-allowed">
