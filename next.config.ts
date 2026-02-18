0b8ea10df60003e36574da9dc4a1d8bbdc89485b
Nabeel John <nabeeljohn@outlook.com>
2026-02-18 16:57:44 -0500

    remove static export config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  output: 'export', // ✅ tells Next.js to export static files
-  images: {
-    unoptimized: true, // ✅ allows images to work on static sites
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
