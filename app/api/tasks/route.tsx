f1149ac5e97d73f5b2e0b43d2a9e9d72e82eaa29
Nabeel John <nabeeljohn@outlook.com>
2026-03-03 23:38:46 -0500

    comment out tasks route

--- app/api/tasks/route.tsx
+++ app/api/tasks/route.tsx
@@ -1,31 +1,31 @@
-import { NextResponse } from "next/server";
-import pkg from "pg";
+// import { NextResponse } from "next/server";
+// import pkg from "pg";
 
-const { Client } = pkg;
+// const { Client } = pkg;
 
-// helper delay function
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// // helper delay function
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export async function GET() {
-  const client = new Client({
-    connectionString: process.env.NJOHNWEB_DATABASE_URL,
-  });
-    try {
-    await client.connect();
+// export async function GET() {
+//   const client = new Client({
+//     connectionString: process.env.NJOHNWEB_DATABASE_URL,
+//   });
+//     try {
+//     await client.connect();
 
-    const res = await client.query(
-      "SELECT taskid, title, description, createdon FROM tasks"
-    );
-    // Add 5 second delay
-    await delay(5000);
-    await client.end();
-    console.log("Database query result:", res.rows);
-    return NextResponse.json(res.rows);
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json(
-      { error: "Database query failed" },
-      { status: 500 }
-    );
-  }
-}
+//     const res = await client.query(
+//       "SELECT taskid, title, description, createdon FROM tasks"
+//     );
+//     // Add 5 second delay
+//     await delay(5000);
+//     await client.end();
+//     console.log("Database query result:", res.rows);
+//     return NextResponse.json(res.rows);
+//   } catch (err) {
+//     console.error(err);
+//     return NextResponse.json(
+//       { error: "Database query failed" },
+//       { status: 500 }
+//     );
+//   }
+// }
