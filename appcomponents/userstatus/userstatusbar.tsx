acca974bd0a20fbc7bfcb46fd9a06fd794953ff1
Nabeel John <nabeeljohn@outlook.com>
2026-04-08 23:46:30 -0400

    z-index fix on user menu

--- appcomponents/userstatus/userstatusbar.tsx
+++ appcomponents/userstatus/userstatusbar.tsx
@@ -2,7 +2,7 @@ import UserStatus from "./userstatus";
 
 export default function UserStatusBar() {
     return (
-        <div className="text-right pt-1 pb-1 bg-gray-900 text-gray-100">
+        <div className="relative z-10 text-right pt-1 pb-1 bg-gray-900 text-gray-100">
                 <UserStatus />
         </div>
     );
